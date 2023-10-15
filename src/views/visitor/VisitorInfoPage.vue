854f46fc33f4d302fbd9bf2b52b8aab342ae42bc
绝恋。。 <1105033640@qq.com>
2023-10-15 15:07:49 +0800

    二维码搞大点儿

--- src/views/visitor/VisitorInfoPage.vue
+++ src/views/visitor/VisitorInfoPage.vue
@@ -13,7 +13,7 @@ const qrcode_url = ref(`${baseurl}/visitor-arrive/${route.params.invite_id}`)
     <van-grid :column-num="1" direction="horizontal">
       <van-grid-item> <van-icon name="smile-o" />欢迎您的到访~ </van-grid-item>
       <van-grid-item>
-        <vue-qr :text="qrcode_url" :size="200"></vue-qr>
+        <vue-qr :text="qrcode_url" :size="300"></vue-qr>
       </van-grid-item>
       <van-grid-item>
         <div>您可以截图保存此二维码，到达时将该二维码出示给门卫扫码！</div>
