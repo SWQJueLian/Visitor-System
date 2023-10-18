4c1bf4ead799678fa9e791c9487de345a7bc0053
绝恋。。 <1105033640@qq.com>
2023-10-18 21:49:47 +0800

    不需要pinia持久化存储到cookie

--- src/stores/modules/user.js
+++ src/stores/modules/user.js
@@ -2,24 +2,17 @@ import { defineStore } from 'pinia'
 // eslint-disable-next-line no-unused-vars
 import { ref } from 'vue'
 
-export const useUserStore = defineStore(
-  'User',
-  () => {
-    // 用户基础信息
-    const userinfo = ref({})
+export const useUserStore = defineStore('User', () => {
+  // 用户基础信息
+  const userinfo = ref({})
 
-    // 设置用户基础信息
-    const setUserinfo = (value) => {
-      userinfo.value = value
-    }
-
-    return {
-      userinfo,
-      setUserinfo
-    }
-  },
-  {
-    // 持久化存储到cookies
-    persist: true
+  // 设置用户基础信息
+  const setUserinfo = (value) => {
+    userinfo.value = value
   }
-)
+
+  return {
+    userinfo,
+    setUserinfo
+  }
+})
